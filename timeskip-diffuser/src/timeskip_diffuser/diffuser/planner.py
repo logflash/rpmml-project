4025117f52602f4c1d8603b3be5b12c3f92e9584
Tomasz A. Sadowy <ts4953@adroit-vis.princeton.edu>
2025-12-22 12:25:30 -0500

    script_vis.ipynb

--- timeskip-diffuser/src/timeskip_diffuser/diffuser/planner.py
+++ timeskip-diffuser/src/timeskip_diffuser/diffuser/planner.py
@@ -222,17 +222,14 @@ def expand_spline_from_skip_list(skip_list, dt=0.01):
         # SCALE velocities to spline coordinates
         v0_scaled = velocities[i] * T
         v1_scaled = velocities[i + 1] * T
-
-        num_samples = np.ceil(k).astype(int) + 1  # +1 to include endpoint
-
+        #Fixed negative predicted timeskips by clamping to positive - Tomasz
         if not np.isfinite(k) or k <= 0:
+            print("k invalid")
             k = 0
-<<<<<<< HEAD
             #Don't raise error anymore
             #raise ValueError(f"Invalid skip k={k} at segment {i}")
-=======
-            # raise ValueError(f"Invalid skip k={k} at segment {i}")
->>>>>>> 4100454db4ac7186c0ef387f5eecb64f0afd63a2
+        num_samples = np.ceil(k).astype(int) + 1  # +1 to include endpoint
+
         P, V, A = hermite_segment(p0, v0_scaled, p1, v1_scaled, num_samples)
 
         # avoid duplication at segment seam
